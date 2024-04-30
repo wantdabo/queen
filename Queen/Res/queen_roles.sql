ceaea9f9a7d5d1afd370d240a79b679789eba666
GoHo <wantdabo@gmail.com>
2024-04-30 11:49:02 +0800

    Update queen_roles.sql

--- Queen/Res/queen_roles.sql
+++ Queen/Res/queen_roles.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `queen` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `queen`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: queen
@@ -52,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-21  3:50:03
+-- Dump completed on 2024-04-30 11:48:25
