286049ac09ba2ccac35e89f0e0be3909fde7533d
GoHo <wantdabo@gmail.com>
2024-09-05 22:45:01 +0800

    Update Sys.cs

--- Queen.Server/System/Sys.cs
+++ Queen.Server/System/Sys.cs
@@ -30,56 +30,56 @@ public abstract class Sys<TAdapter> : Sys where TAdapter : Adapter, new()
 
     protected override void OnCreate()
     {
-            base.OnCreate();
-            adapter = AddComp<TAdapter>();
-            adapter.Initialize(this);
-            adapter.Create();
-            NetRecv();
-        }
+        base.OnCreate();
+        adapter = AddComp<TAdapter>();
+        adapter.Initialize(this);
+        adapter.Create();
+        NetRecv();
+    }
 
     protected override void OnDestroy()
     {
-            base.OnDestroy();
-            NetUnRecv();
-        }
+        base.OnDestroy();
+        NetUnRecv();
+    }
 
     /// <summary>
     /// 自动注册消息接收
     /// </summary>
     private void NetRecv()
     {
-            foreach (var method in adapter.GetType().GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance))
+        foreach (var method in adapter.GetType().GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance))
+        {
+            if (null == method.GetCustomAttribute<NetBinding>()) continue;
+            var ps = method.GetParameters();
+            if (2 != ps.Length) continue;
+            if (ps[0].ParameterType != typeof(NetChannel)) continue;
+            var msgType = ps[1].ParameterType;
+            if (false == typeof(INetMessage).IsAssignableFrom(msgType)) continue;
+            var actionType = typeof(Action<,>).MakeGenericType(typeof(NetChannel), msgType);
+            var action = Delegate.CreateDelegate(actionType, adapter, method);
+            var actionInfo = new ActionInfo
             {
-                if(null == method.GetCustomAttribute<NetBinding>()) continue;
-                var ps = method.GetParameters();
-                if (2 != ps.Length) continue;
-                if (ps[0].ParameterType != typeof(NetChannel)) continue;
-                var msgType = ps[1].ParameterType;
-                if (false == typeof(INetMessage).IsAssignableFrom(msgType)) continue;
-                var actionType = typeof(Action<,>).MakeGenericType(typeof(NetChannel), msgType);
-                var action = Delegate.CreateDelegate(actionType, adapter, method);
-                var actionInfo = new ActionInfo
-                {
-                    msgType = msgType,
-                    action = action
-                };
-                adapter.actionInfos.Add(actionInfo);
+                msgType = msgType,
+                action = action
+            };
+            adapter.actionInfos.Add(actionInfo);
 
-                var recvMethod = engine.slave.GetType().GetMethod("Recv").MakeGenericMethod(msgType);
-                recvMethod.Invoke(engine.slave, [action]);
-            }
+            var recvMethod = engine.slave.GetType().GetMethod("Recv").MakeGenericMethod(msgType);
+            recvMethod.Invoke(engine.slave, [action]);
         }
+    }
 
     /// <summary>
     /// 自动注销消息接收
     /// </summary>
     private void NetUnRecv()
     {
-            foreach (var actionInfo in adapter.actionInfos)
-            {
-                var recvMethod = engine.slave.GetType().GetMethod("UnRecv").MakeGenericMethod(actionInfo.msgType);
-                recvMethod.Invoke(engine.slave, [actionInfo.action]);
-            }
-            adapter.actionInfos.Clear();
+        foreach (var actionInfo in adapter.actionInfos)
+        {
+            var recvMethod = engine.slave.GetType().GetMethod("UnRecv").MakeGenericMethod(actionInfo.msgType);
+            recvMethod.Invoke(engine.slave, [actionInfo.action]);
         }
+        adapter.actionInfos.Clear();
+    }
 }
