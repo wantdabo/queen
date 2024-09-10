d6711e9cc2ce8e7761f4375084a1ded0af9375df
GoHo <wantdabo@gmail.com>
2024-09-10 16:11:04 +0800

    Update Coroutine.cs

--- Queen/Common/Parallel/Coroutine.cs
+++ Queen/Common/Parallel/Coroutine.cs
@@ -14,25 +14,29 @@ public class Coroutine
     /// <summary>
     /// 协程驱动器
     /// </summary>
-    private CoroutineScheduler scheduler { get; set; }
+    private CoroutineScheduler coroutines { get; set; }
+    /// <summary>
+    /// 协程工作中
+    /// </summary>
+    public bool working { get; private set; }
     /// <summary>
     /// 逻辑分片
     /// </summary>
     private IEnumerator<Instruction> enumerator { get; set; }
-        
+
     /// <summary>
     /// 设置参数
     /// </summary>
     /// <param name="id">协程 ID</param>
-    /// <param name="scheduler">协程驱动</param>
+    /// <param name="coroutines">协程驱动</param>
     /// <param name="enumerator">逻辑分片</param>
-    public void Reset(uint id, CoroutineScheduler scheduler, IEnumerator<Instruction> enumerator)
+    public void Reset(uint id, CoroutineScheduler coroutines, IEnumerator<Instruction> enumerator)
     {
         this.id = id;
-        this.scheduler = scheduler;
+        this.coroutines = coroutines;
         this.enumerator = enumerator;
     }
-        
+
     /// <summary>
     /// 协程指令就绪检查
     /// </summary>
@@ -53,13 +57,15 @@ public class Coroutine
         if (null == enumerator.Current) return;
         enumerator.Current.Update(tick);
     }
-        
+
     /// <summary>
     /// 执行协程
     /// </summary>
     /// <returns>YES/NO (true 表示逻辑分片未结束/ 否则就是逻辑分片已结束)</returns>
     public bool Execute()
     {
-        return enumerator.MoveNext();
+        working = enumerator.MoveNext();
+
+        return working;
     }
 }
