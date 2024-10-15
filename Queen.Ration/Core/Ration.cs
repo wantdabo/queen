2b06732a7b4b0b5af039dabafb0120e2c22b2dad
GoHo <wantdabo@gmail.com>
2024-10-15 18:09:19 +0800

    加入 RPC

--- Queen.Ration/Core/Ration.cs
+++ Queen.Ration/Core/Ration.cs
@@ -1,4 +1,5 @@
 using Queen.Core;
+using Queen.Network.Cross;
 
 namespace Queen.Ration.Core;
 
@@ -7,7 +8,8 @@ public class Ration : Engine<Ration>
     /// <summary>
     /// 服务器配置
     /// </summary>
-    public Settings settings { get; set; }
+    public Settings settings { get; private set; }
+    public RPC rpc { get; private set; }
 
     protected override void OnCreate()
     {
@@ -15,6 +17,10 @@ public class Ration : Engine<Ration>
         settings = AddComp<Settings>();
         settings.Create();
         
+        rpc = AddComp<RPC>();
+        rpc.Initialize(settings.rpchost, settings.rpcport, settings.rpcidlecc, settings.rpctimeout, settings.rpcdeadtime);
+        rpc.Create();
+
         engine.logger.Info(
             $"\n\tname: {settings.name}"
             , ConsoleColor.Yellow);
--- Queen.Ration/Settings.cs
+++ Queen.Ration/Settings.cs
@@ -9,12 +9,37 @@ public class Settings : Comp
     /// 服务器名字
     /// </summary>
     public string name { get; private set; }
+    /// <summary>
+    /// RPC 主机
+    /// </summary>
+    public string rpchost { get; private set; }
+    /// <summary>
+    /// RPC 端口
+    /// </summary>
+    public ushort rpcport { get; private set; }
+    /// <summary>
+    /// RPC 闲置等待的数量（库存通信用的 RPC.Clinet）
+    /// </summary>
+    public ushort rpcidlecc { get; private set; }
+    /// <summary>
+    /// RPC 超时设定
+    /// </summary>
+    public uint rpctimeout { get; private set; }
+    /// <summary>
+    /// RPC 超时延长，目标如果收到 RPC 消息 ACK，将会进入这个等待时长
+    /// </summary>
+    public uint rpcdeadtime { get; private set; }
 
     protected override void OnCreate()
     {
         base.OnCreate();
         var jobj = JObject.Parse(File.ReadAllText($"{Directory.GetCurrentDirectory()}/Res/settings.json"));
         name = jobj.Value<string>("name");
+        rpchost = jobj.Value<string>("rpchost");
+        rpcport = jobj.Value<ushort>("rpcport");
+        rpcidlecc = jobj.Value<ushort>("rpcidlecc");
+        rpctimeout = jobj.Value<uint>("rpctimeout");
+        rpcdeadtime = jobj.Value<uint>("rpcdeadtime");
     }
 
     protected override void OnDestroy()
