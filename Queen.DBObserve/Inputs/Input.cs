2c5b9718c22b9d1b8f381c6412ca96b13ca9ad23
GoHo <wantdabo@gmail.com>
2024-10-18 17:37:29 +0800

    Update Input.cs

--- Queen.DBObserve/Inputs/Input.cs
+++ Queen.DBObserve/Inputs/Input.cs
@@ -12,28 +12,28 @@ public class Input : Comp
 {
     protected override void OnCreate()
     {
-            base.OnCreate();
-            engine.eventor.Listen<ExecuteEvent>(OnExecute);
-        }
+        base.OnCreate();
+        engine.eventor.Listen<ExecuteEvent>(OnExecute);
+    }
 
     protected override void OnDestroy()
     {
-            base.OnDestroy();
-            engine.eventor.UnListen<ExecuteEvent>(OnExecute);
-        }
+        base.OnDestroy();
+        engine.eventor.UnListen<ExecuteEvent>(OnExecute);
+    }
 
     private void OnExecute(ExecuteEvent e)
     {
-            RecvInput();
-        }
+        RecvInput();
+    }
 
     private void RecvInput()
     {
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write($"mongo@{engine.dbo.dbname}:{engine.dbo.dbhost}> ");
-            var instruction = Console.ReadLine();
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write($"mongo@{engine.dbo.dbname}:{engine.dbo.dbhost}> ");
+        var instruction = Console.ReadLine();
 
-            if (string.IsNullOrEmpty(instruction)) return;
-            engine.resolver.Execute(instruction);
-        }
+        if (string.IsNullOrEmpty(instruction)) return;
+        engine.resolver.Execute(instruction);
+    }
 }
